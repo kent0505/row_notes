2f31c623242fa2d3d34b78e62dc0b0a02972958c
Otabek <78997827+kent0505@users.noreply.github.com>
2023-07-29 11:56:13 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import './cubit/note_cubit.dart';
-import './cubit/test_cubit.dart';
 import './pages/home/home_page.dart';
-import './pages/test/test_page.dart';
-import './pages/pincode/pincode_page.dart';
 import './logic/themes.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('mybox');
-  // Hive.box('mybox').clear();
+
   await Future.delayed(const Duration(seconds: 1));
   runApp(const MyApp());
 }
@@ -24,25 +21,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Themes themes = Themes();
 
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => NoteCubit({})..restoreData(),
-        ),
-        BlocProvider(
-          create: (context) => TestCubit([]),
-        ),
-      ],
+    return BlocProvider(
+      create: (context) => NoteCubit({})..restoreData(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: themes.darkTheme,
         home: const HomePage(),
-        // home: const TestPage(),
         initialRoute: '/',
         routes: {
           '/home': (context) => const HomePage(),
-          '/test': (context) => const TestPage(),
-          '/pincode': (context) => const PincodePage(),
         },
       ),
     );
